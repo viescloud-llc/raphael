d334fe75d842ea67f20d1683ac033e14b6d84d6e
VincentB00 <vincentbui1999@gmail.com>
2026-04-19 13:22:11 -0400

    feat(config): Add ViesApplicationConfig bean and update K8s load balancer IP

--- src/main/java/com/vincent/inc/raphael/config/BeanConfig.java
+++ src/main/java/com/vincent/inc/raphael/config/BeanConfig.java
@@ -3,15 +3,27 @@ package com.vincent.inc.raphael.config;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.viescloud.eco.viesspringutils.auto.config.ViesApplicationConfig;
+import com.viescloud.eco.viesspringutils.auto.model.authentication.ViesDefaultEndpointEnum;
+
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class BeanConfig {
+    @Bean
+    public ViesApplicationConfig viesApplicationConfig(@Value("${spring.profiles.active:local}") String env) {
+        var config = new ViesApplicationConfig(env, ViesDefaultEndpointEnum.toList());
+        config.setEnabledSecuity(false);
+        return config;
+    }
+
     @Bean
     public CorsWebFilter corsWebFilter() {
         final CorsConfiguration corsConfig = new CorsConfiguration();
